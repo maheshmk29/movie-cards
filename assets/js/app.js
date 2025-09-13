33b4fd3237738d2808df49bc3329b940f3f741c5
mahesh <maheshkambar728@gmail.com>
2025-09-14 03:20:51 +0530

    create movie cards

---
+++ assets/js/app.js
@@ -0,0 +1,56 @@
+const cl =console.log;
+const moviecontainer=document.getElementById('moviecontainer')
+
+
+const setRating=(rating) =>{
+	if (rating >=7){
+		return ' badge-success '
+	}else if (rating >=5 && rating< 7){
+		return ' badge-warning'
+	}else{
+		return ' badge-danger'
+	}
+}
+
+let result=' '
+movieArray.forEach(movie =>{
+	result+=`<div class="col-md-4  mb-4">
+			       <div class="card">
+			<figure class="moviecard mb-0">
+			<!-- <-----------img ------------------> 
+			<img src="https://image.tmdb.org/t/p/w500/${movie.backdrop_path || movie.poster_path}"
+			title="${movie.title}"
+			alt="${movie.title}">
+
+		<figcaption>
+			 <div class="titleInfo">
+			    <div class="row">
+			        <div class="col-10">
+			           <h3>
+					   ${movie.title|| movie.original_title}
+					   </h3>
+			               </div>
+			                  <div class="col-2">
+			  
+			 
+		              	<h3><span class="badge${setRating(movie.vote_average)}">*${movie.vote_average}</span> </h3>
+			       </div>
+			    </div>
+			  </div>
+			  
+			  <div class="overview">
+			  <h5>
+			  ${movie.title}
+			  </h5>
+			  <p>  
+			  ${movie.overview} 
+			  </p>
+			</figcaption>
+		  </figure>
+		 </div>
+	   </div>
+	</div>
+	</div>` 
+})
+
+moviecontainer.innerHTML =result;
---
+++ assets/js/data.js
@@ -0,0 +1,84 @@
+const movieArray= [
+  {
+    
+    backdrop_path:"/image.tmdb.org/t/p/w500/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg",
+    title:"OFFICER",
+    overview: "A crew sets sail on a dangerous journey across uncharted waters, facing storms and hidden threats.",
+	vote_average:7.5,
+  },
+  {
+     poster_path: "/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg",
+    backdrop_path: "/rr7E0NoGKxvbkb89eR1GwfoYjpA.jpg",
+    vote_average:5,
+    title: "Fight Club",
+    overview: "In a city ruled by darkness, a group of rebels rise to fight against an ancient evil.",
+  },
+  { 
+     title: "The Dark Knight",
+	 vote_average:4,
+    poster_path: "/qJ2tW6WMUDux911r6m7haRef0WH.jpg",
+    backdrop_path: "/hkBaDkMWbLaf8B1lsWsKX7Ew3Xq.jpg",
+	overview: "In a city ruled by darkness, a group of rebels rise to fight against an ancient evil.",
+  },
+  {
+    
+   title: "Avengers: Infinity War",
+   vote_average:9.5,
+    poster_path: "/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg",
+    backdrop_path: "/bOGkgRGdhrBYJSLpXaxhXVstddV.jpg",
+    overview: "An epic space battle unfolds as humans and aliens clash for the future of the galaxy.",
+  },
+  
+  {
+    
+    backdrop_path:"/image.tmdb.org/t/p/w500/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg",
+    title:"OFFICER",
+    overview: "A crew sets sail on a dangerous journey across uncharted waters, facing storms and hidden threats.",
+	vote_average:7.5,
+  },
+  {
+     poster_path: "/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg",
+    backdrop_path: "/rr7E0NoGKxvbkb89eR1GwfoYjpA.jpg",
+    vote_average:5,
+    title: "Fight Club",
+    overview: "In a city ruled by darkness, a group of rebels rise to fight against an ancient evil.",
+  },
+  { 
+     title: "The Dark Knight",
+	 vote_average:4,
+    poster_path: "/qJ2tW6WMUDux911r6m7haRef0WH.jpg",
+    backdrop_path: "/hkBaDkMWbLaf8B1lsWsKX7Ew3Xq.jpg",
+	overview: "In a city ruled by darkness, a group of rebels rise to fight against an ancient evil.",
+  },
+  {
+    
+   title: "Avengers: Infinity War",
+   vote_average:9.5,
+    poster_path: "/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg",
+    backdrop_path: "/bOGkgRGdhrBYJSLpXaxhXVstddV.jpg",
+    overview: "An epic space battle unfolds as humans and aliens clash for the future of the galaxy.",
+  },
+  
+  {
+     poster_path: "/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg",
+    backdrop_path: "/rr7E0NoGKxvbkb89eR1GwfoYjpA.jpg",
+    vote_average:5,
+    title: "Fight Club",
+    overview: "In a city ruled by darkness, a group of rebels rise to fight against an ancient evil.",
+  },
+  { 
+     title: "The Dark Knight",
+	 vote_average:4,
+    poster_path: "/qJ2tW6WMUDux911r6m7haRef0WH.jpg",
+    backdrop_path: "/hkBaDkMWbLaf8B1lsWsKX7Ew3Xq.jpg",
+	overview: "In a city ruled by darkness, a group of rebels rise to fight against an ancient evil.",
+  },
+  {
+    
+   title: "Avengers: Infinity War",
+   vote_average:9.5,
+    poster_path: "/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg",
+    backdrop_path: "/bOGkgRGdhrBYJSLpXaxhXVstddV.jpg",
+    overview: "An epic space battle unfolds as humans and aliens clash for the future of the galaxy.",
+  }
+];
