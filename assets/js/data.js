f433de1dccdf25c71f5f00247a55d172ea0a115d
mahesh <maheshkambar728@gmail.com>
2025-09-14 12:41:32 +0530

    created movie cards

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,84 +1,63 @@
 const movieArray= [
-  {
-    
-    backdrop_path:"/image.tmdb.org/t/p/w500/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg",
-    title:"OFFICER",
-    overview: "A crew sets sail on a dangerous journey across uncharted waters, facing storms and hidden threats.",
-	vote_average:7.5,
-  },
-  {
-     poster_path: "/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg",
-    backdrop_path: "/rr7E0NoGKxvbkb89eR1GwfoYjpA.jpg",
-    vote_average:5,
-    title: "Fight Club",
-    overview: "In a city ruled by darkness, a group of rebels rise to fight against an ancient evil.",
-  },
-  { 
-     title: "The Dark Knight",
-	 vote_average:4,
-    poster_path: "/qJ2tW6WMUDux911r6m7haRef0WH.jpg",
-    backdrop_path: "/hkBaDkMWbLaf8B1lsWsKX7Ew3Xq.jpg",
-	overview: "In a city ruled by darkness, a group of rebels rise to fight against an ancient evil.",
-  },
-  {
-    
-   title: "Avengers: Infinity War",
-   vote_average:9.5,
-    poster_path: "/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg",
-    backdrop_path: "/bOGkgRGdhrBYJSLpXaxhXVstddV.jpg",
-    overview: "An epic space battle unfolds as humans and aliens clash for the future of the galaxy.",
+ { 
+    title: "Shutter Island", 
+    overview: "A U.S. Marshal investigates the disappearance of a murderer who escaped from a hospital for the criminally insane.", 
+    vote_average: 8.2, 
+     backdrop_path: "https://image.tmdb.org/t/p/w500/kve20tXwUZpu4GUX8l6X7Z4jmL6.jpg" 
   },
   
-  {
-    
-    backdrop_path:"/image.tmdb.org/t/p/w500/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg",
-    title:"OFFICER",
-    overview: "A crew sets sail on a dangerous journey across uncharted waters, facing storms and hidden threats.",
-	vote_average:7.5,
+  { 
+    title: "Fight Club", 
+    overview: "An insomniac office worker and a soapmaker form an underground fight club that evolves into something much more.", 
+    vote_average: 4, 
+     backdrop_path: "https://image.tmdb.org/t/p/w500/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg" 
   },
-  {
-     poster_path: "/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg",
-    backdrop_path: "/rr7E0NoGKxvbkb89eR1GwfoYjpA.jpg",
-    vote_average:5,
-    title: "Fight Club",
-    overview: "In a city ruled by darkness, a group of rebels rise to fight against an ancient evil.",
+   { 
+    title: "Titanic", 
+    overview: "A seventeen-year-old aristocrat falls in love with a kind but poor artist aboard the luxurious Titanic.", 
+   vote_average: 5.8, 
+     backdrop_path: "https://image.tmdb.org/t/p/w500/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", 
   },
   { 
-     title: "The Dark Knight",
-	 vote_average:4,
-    poster_path: "/qJ2tW6WMUDux911r6m7haRef0WH.jpg",
-    backdrop_path: "/hkBaDkMWbLaf8B1lsWsKX7Ew3Xq.jpg",
-	overview: "In a city ruled by darkness, a group of rebels rise to fight against an ancient evil.",
-  },
-  {
-    
-   title: "Avengers: Infinity War",
-   vote_average:9.5,
-    poster_path: "/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg",
-    backdrop_path: "/bOGkgRGdhrBYJSLpXaxhXVstddV.jpg",
-    overview: "An epic space battle unfolds as humans and aliens clash for the future of the galaxy.",
-  },
-  
-  {
-     poster_path: "/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg",
-    backdrop_path: "/rr7E0NoGKxvbkb89eR1GwfoYjpA.jpg",
-    vote_average:5,
-    title: "Fight Club",
-    overview: "In a city ruled by darkness, a group of rebels rise to fight against an ancient evil.",
+    title: "The Wolf of Wall Street", 
+    overview: "Based on the true story of Jordan Belfort, a wealthy stockbroker who falls into a world of crime and corruption.", 
+    vote_average: 5.5, 
+     backdrop_path: "https://image.tmdb.org/t/p/w500/sOxr33wnRuKazR9ClHek73T8qnK.jpg" ,
   },
   { 
-     title: "The Dark Knight",
-	 vote_average:4,
-    poster_path: "/qJ2tW6WMUDux911r6m7haRef0WH.jpg",
-    backdrop_path: "/hkBaDkMWbLaf8B1lsWsKX7Ew3Xq.jpg",
-	overview: "In a city ruled by darkness, a group of rebels rise to fight against an ancient evil.",
+    title: "Whiplash", 
+    overview: "A promising young drummer enrolls at a cut-throat music conservatory where his dreams are mentored by an abusive instructor.", 
+   vote_average: 6.5, 
+     backdrop_path: "https://image.tmdb.org/t/p/w500/oPxnRhyAIzJKGUEdSiwTJQBa3NM.jpg" ,
   },
-  {
-    
-   title: "Avengers: Infinity War",
-   vote_average:9.5,
-    poster_path: "/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg",
-    backdrop_path: "/bOGkgRGdhrBYJSLpXaxhXVstddV.jpg",
-    overview: "An epic space battle unfolds as humans and aliens clash for the future of the galaxy.",
-  }
-];
+  { 
+    title: "The Social Network", 
+    overview: "The story of how Mark Zuckerberg created Facebook and faced legal and personal challenges.", 
+   vote_average: 8.8, 
+     backdrop_path: "https://image.tmdb.org/t/p/w500/n0ybibhJtQ5icDqTp8eRytcIHJx.jpg", 
+  },
+  { 
+    title: "Parasite", 
+    overview: "A poor family schemes to become employed by a wealthy household and infiltrates their lives.", 
+    vote_average: 6, 
+     backdrop_path: "https://image.tmdb.org/t/p/w500/7IiTTgloJzvGI1TAYymCfbfl3vT.jpg" ,
+  },
+  { 
+    title: "Mad Max: Fury Road", 
+    overview: "In a post-apocalyptic wasteland, Max teams up with Furiosa to flee from a tyrant and his army.", 
+  vote_average: 7.8, 
+     backdrop_path: "https://image.tmdb.org/t/p/w500/8tZYtuWezp8JbcsvHYO0O46tFbo.jpg" ,
+  },
+  { 
+    title: "La La Land", 
+    overview: "A jazz musician and an aspiring actress fall in love but struggle to maintain their relationship while pursuing dreams.", 
+   vote_average: 6, 
+     backdrop_path: "https://image.tmdb.org/t/p/w500/uDO8zWDhfWwoFdKS4fzkUJt0Rf0.jpg" ,
+  },
+  { 
+    title: "Django Unchained", 
+    overview: "With the help of a German bounty hunter, a freed slave sets out to rescue his wife from a brutal plantation owner.", 
+    vote_average: 5, 
+     backdrop_path: "https://image.tmdb.org/t/p/w500/7oWY8VDWW7thTzWh3OKYRkWUlD5.jpg", 
+  } 
+]
